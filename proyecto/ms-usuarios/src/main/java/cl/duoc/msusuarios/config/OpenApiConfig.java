886090bc5c6e564f635bd39e33cf737661e0a2aa
Marcial-Lagos <marci.lagosp@duocuc.cl>
2026-06-25 02:51:31 -0400

    Fix OpenAPI server URL for Gateway Swagger

--- proyecto/ms-usuarios/src/main/java/cl/duoc/msusuarios/config/OpenApiConfig.java
+++ proyecto/ms-usuarios/src/main/java/cl/duoc/msusuarios/config/OpenApiConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -18,6 +21,11 @@ public class OpenApiConfig {
                         .title("FoodExpress - Usuarios")
                         .version("1.0.0")
                         .description("Gestión de perfiles de usuario."))
+                .servers(List.of(
+                        new Server()
+                                .url("/")
+                                .description("Servidor relativo para Swagger vía Gateway")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components().addSecuritySchemes("bearerAuth",
                         new SecurityScheme()
