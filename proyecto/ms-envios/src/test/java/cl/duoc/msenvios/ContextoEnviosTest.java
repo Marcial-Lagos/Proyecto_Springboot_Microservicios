2e7767483b984fe8e6165f214a2c3f1520400ab9
Marcial-Lagos <marci.lagosp@duocuc.cl>
2026-06-25 06:30:32 -0400

    test(envios): comprobar carga del contexto

---
+++ proyecto/ms-envios/src/test/java/cl/duoc/msenvios/ContextoEnviosTest.java
@@ -0,0 +1,13 @@
+package cl.duoc.msenvios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ContextoEnviosTest {
+    @Test
+    void iniciaAplicacion() {
+    }
+}
