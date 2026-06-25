fd4a9724af89efc2ebde5b6505da2964a607fa38
Marcial-Lagos <marci.lagosp@duocuc.cl>
2026-06-25 06:35:53 -0400

    test(cupones): verificar contexto

---
+++ proyecto/ms-cupones/src/test/java/cl/duoc/mscupones/AplicacionCuponesTest.java
@@ -0,0 +1,13 @@
+package cl.duoc.mscupones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AplicacionCuponesTest {
+    @Test
+    void pruebaContexto() {
+    }
+}
